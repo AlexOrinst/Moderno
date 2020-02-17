8b249a4eadbebf5d668eca43e79d5083de138509
AlexOrinst <koluchiy8@gmail.com>
2020-02-17 11:45:24 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,17 @@
 $(function(){
     var mixer = mixitup('.products__inner-box');
+   
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: '12px',
+        readOnly: true
+      });
 
-
-    
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToScroll: 4,
+        slidesToShow: 4
+    });
 
 });
