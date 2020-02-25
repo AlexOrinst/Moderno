6e51c4bc03f2b4c21f6c9879485de0c6cebb50ef
AlexOrinst <koluchiy8@gmail.com>
2020-02-25 21:00:22 +0300

    end new-items

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function(){
+
+if ($('.products__inner-box').length) {
     var mixer = mixitup('.products__inner-box');
+  };
    
     $('.rate-star').rateYo({
         rating: 5,
@@ -14,4 +17,12 @@ $(function(){
         slidesToShow: 4
     });
 
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: "$"
+    });
 });
