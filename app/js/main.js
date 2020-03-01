6a63346d2231ab13db29d09b64268368248d6963
AlexOrinst <koluchiy8@gmail.com>
2020-03-01 20:18:04 +0300

    adaptive header&footer

--- app/js/main.js
+++ app/js/main.js
@@ -37,5 +37,15 @@ if ($('.products__inner-box').length) {
       $('.icon-th-list').removeClass('active')
     });
 
+    $('.menu__btn').on('click', function(){
+      $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function(){
+      $('.header__box').toggleClass('active');
+    });
+
+    
+
 
 });
